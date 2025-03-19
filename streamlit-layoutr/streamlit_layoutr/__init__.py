251f901c992a36762c023fdd717e35e6498efee3
Jeffrey Baumes <jeff.baumes@kitware.com>
2025-03-19 15:03:45 -0400

    factor out library and create streamlit component

---
+++ streamlit-layoutr/streamlit_layoutr/__init__.py
@@ -0,0 +1,82 @@
+import os
+
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        "streamlit_layoutr",
+        url="http://localhost:5173",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/dist")
+    _component_func = components.declare_component("streamlit_layoutr", path=build_dir)
+
+
+def streamlit_layoutr(
+    graph=None,
+    run_layout=False,
+    node_size=1,
+    node_size_field=None,
+    node_color_field=None,
+    node_opacity=1,
+    node_stroke_width=1,
+    node_stroke_opacity=1,
+    link_width=1,
+    link_opacity=1,
+    energy=1,
+    collide_strength=1,
+    charge_strength=1,
+    charge_approximation=1,
+    link_strength=1,
+    gravity_strength=0,
+    center_strength=1,
+    key=None,
+):
+    """Create a new instance of "streamlit_layoutr".
+
+    Parameters
+    ----------
+    graph: str
+        The graph data in string format to be processed by the component.
+    run_layout: bool
+        A flag indicating whether to run the layout.
+    link_strength: float
+        A value representing the strength of the links in the graph.
+    node_size: float
+        A value representing the node size.
+    key: str or None
+        An optional key that uniquely identifies this component. If this is
+        None, and the component's arguments are changed, the component will
+        be re-mounted in the Streamlit frontend and lose its current state.
+
+    Returns
+    -------
+    int
+        The number of times the component's "Click Me" button has been clicked.
+    """
+    component_value = _component_func(
+        graph=graph,
+        run_layout=run_layout,
+        node_size=node_size,
+        node_size_field=node_size_field,
+        node_color_field=node_color_field,
+        node_opacity=node_opacity,
+        node_stroke_width=node_stroke_width,
+        node_stroke_opacity=node_stroke_opacity,
+        link_width=link_width,
+        link_opacity=link_opacity,
+        energy=energy,
+        collide_strength=collide_strength,
+        charge_strength=charge_strength,
+        charge_approximation=charge_approximation,
+        link_strength=link_strength,
+        gravity_strength=gravity_strength,
+        center_strength=center_strength,
+        key=key,
+        default=dict(fields=[]),
+    )
+
+    return component_value
---
+++ streamlit-layoutr/streamlit_layoutr/frontend/src/StreamlitLayoutr.vue
@@ -0,0 +1,91 @@
+<template>
+  <canvas ref="canvas" width="100" height="100" :style="{ width: '100%', height: '100%' }"></canvas>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from "vue"
+import { Streamlit } from "streamlit-component-lib"
+import { useStreamlit } from "./streamlit"
+import { Layout } from "./lib/index"
+import type { Node, SerializedGraph } from "./lib/types";
+
+const props = defineProps<{
+  args: {
+    graph: string;
+
+    run_layout: boolean;
+    node_size: number;
+    node_size_field: string | null;
+    node_color_field: string | null;
+    node_opacity: number;
+    node_stroke_width: number;
+    node_stroke_opacity: number;
+    link_width: number;
+    link_opacity: number;
+
+    energy: number;
+    collide_strength: number;
+    charge_strength: number;
+    charge_approximation: number;
+    link_strength: number;
+    gravity_strength: number;
+    center_strength: number;
+  };
+}>();
+
+let layout: Layout | null = null;
+let graph: SerializedGraph | null = null;
+let hovered: Node | null = null;
+let selected: Node | null = null;
+const canvas = ref<HTMLCanvasElement | null>(null);
+
+const loadGraph = () => {
+  if (!layout || !props.args.graph) {
+    return;
+  }
+  layout.loadJSON(props.args.graph);
+};
+
+onMounted(() => {
+  if (!canvas.value) {
+    return;
+  }
+  layout = new Layout({
+    canvas: canvas.value,
+    onHover: (hoveredNode) => {
+      hovered = hoveredNode;
+      Streamlit.setComponentValue({graph, hovered, selected});
+    },
+    onSelect: (selectedNode) => {
+      selected = selectedNode;
+      Streamlit.setComponentValue({graph, hovered, selected});
+    },
+  });
+  watch(
+    () => props.args.graph,
+    loadGraph,
+    { immediate: true },
+  );
+
+  watch(() => props.args.run_layout, () => props.args.run_layout ? layout?.start() : layout?.stop(), { immediate: true });
+  watch(() => props.args.node_size, () => layout?.setNodeSize(props.args.node_size), { immediate: true });
+  watch(() => props.args.node_size_field, () => layout?.setNodeSizeField(props.args.node_size_field), { immediate: true });
+  watch(() => props.args.node_color_field, () => layout?.setNodeColorField(props.args.node_color_field), { immediate: true });
+  watch(() => props.args.node_opacity, () => layout?.setNodeOpacity(props.args.node_opacity), { immediate: true });
+  watch(() => props.args.node_stroke_width, () => layout?.setNodeStrokeWidth(props.args.node_stroke_width), { immediate: true });
+  watch(() => props.args.node_stroke_opacity, () => layout?.setNodeStrokeOpacity(props.args.node_stroke_opacity), { immediate: true });
+  watch(() => props.args.link_width, () => layout?.setLinkWidth(props.args.link_width), { immediate: true });
+  watch(() => props.args.link_opacity, () => layout?.setLinkOpacity(props.args.link_opacity), { immediate: true });
+
+  watch(() => props.args.energy, () => layout?.setEnergy(props.args.energy), { immediate: true });
+  watch(() => props.args.collide_strength, () => layout?.setCollideStrength(props.args.collide_strength), { immediate: true });
+  watch(() => props.args.charge_strength, () => layout?.setChargeStrength(props.args.charge_strength), { immediate: true });
+  watch(() => props.args.charge_approximation, () => layout?.setChargeApproximation(props.args.charge_approximation), { immediate: true });
+  watch(() => props.args.link_strength, () => layout?.setLinkStrength(props.args.link_strength), { immediate: true });
+  watch(() => props.args.gravity_strength, () => layout?.setGravityStrength(props.args.gravity_strength), { immediate: true });
+  watch(() => props.args.center_strength, () => layout?.setCenterStrength(props.args.center_strength), { immediate: true });
+});
+
+useStreamlit();
+
+</script>
